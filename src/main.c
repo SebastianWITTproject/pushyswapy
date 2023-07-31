920f1e04b7a5289b9481b59414e646b7e6cd5907
Sebastien WITT <sebi12@hotmail.fr>
2023-07-31 23:58:06 +0200

    push

---
+++ src/main.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcombeau <mcombeau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 14:04:00 by mcombeau          #+#    #+#             */
+/*   Updated: 2022/04/30 15:11:18 by mcombeau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/* is_sorted:
+*	Tant que le node precedent est plus petit que le suivant c'est trie
+	si c'est pas le cas une seule fois c'est pas trie donc retourne faux
+*/
+int	is_sorted(t_stack *stack)
+{
+	while (stack->next != NULL)
+	{
+		if (stack->value > stack->next->value)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+/* push_swap:
+*	Chooses a sorting method depending on the number
+*	of values to be sorted.
+*/
+static void	push_swap(t_stack **stack_a, t_stack **stack_b, int stack_size)
+{
+	if (stack_size == 2 && !is_sorted(*stack_a))
+		do_sa(stack_a);
+	else if (stack_size == 3)
+		tiny_sort(stack_a);
+	else if (stack_size > 3 && !is_sorted(*stack_a))
+		sort(stack_a, stack_b);
+}
+
+/* main:
+*	1) Check si pas d'arg passe
+	2) Verifie que input respecte les conditions du sujet pas de repet + nombre etc.
+	3) Cree une liste chainee representant la pile a avec tous les nombres en param
+	4) Assigne les index 
+	5) Fait le tri 
+	6) Free
+*/
+int	main(int ac, char **av)
+{
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	int		stack_size;
+
+	if (ac < 2)
+		return (0);
+	if (!is_correct_input(av))
+		exit_error(NULL, NULL);
+	stack_b = NULL;
+	stack_a = fill_stack_values(ac, av);
+	stack_size = get_stack_size(stack_a);
+	assign_index(stack_a, stack_size + 1);
+	push_swap(&stack_a, &stack_b, stack_size);
+	free_stack(&stack_a);
+	free_stack(&stack_b);
+	return (0);
+}
